37218f2921543bb27cac07b2e3403808d369430f
Iyan Nazarian <36068362+FxmousKid@users.noreply.github.com>
2025-12-15 13:37:12 +0100

    chore: cleaning parse_elf and playing around with the ELF

--- src/parser/parse_elf.c
+++ src/parser/parse_elf.c
@@ -17,42 +17,39 @@ static bool	parse_elf64(struct s_map *map)
 {
 	const Elf64_Ehdr*	hdr;
 	const Elf64_Shdr*	shdr;
+	int			idx = 0;
 
 	if (!m_inbounds(map, 0, sizeof(Elf64_Ehdr))) {
 		LOG_MSG("File doesn't have a ELF header");
 		return false;
 	}
 
+	// Parse ELF header
 	hdr = (const Elf64_Ehdr *)map->base;
+	if (!hdr->e_shnum || !hdr->e_shoff) {
+		LOG_MSG("No section header in ELF found")
+		return false;
+	}
+	shdr = (const Elf64_Shdr *)(map->base + hdr->e_shoff);
 
-	if (hdr->e_shnum && hdr->e_shoff)
-		shdr = (const Elf64_Shdr *)(map->base + hdr->e_shoff);
-	else
-		shdr = NULL;
-
-	const Elf64_Shdr *tmp;
-	const Elf64_Shdr *symtab_shdr = NULL;
-	const Elf64_Shdr *dyn_shdr = NULL;
-	int idx = -1;
+	// Get .symtab section
+	const Elf64_Shdr *tmp = NULL, *symtab_shdr = NULL;
 	while (shdr && ++idx < hdr->e_shnum) {
 		tmp = shdr + idx;
-		switch(tmp->sh_type) {
-		case (SHT_DYNSYM):
-			dyn_shdr = tmp;
-			break;
-		case (SHT_SYMTAB):
+		if (tmp->sh_type == SHT_SYMTAB)
 			symtab_shdr = tmp;
-			(void)dyn_shdr; // to shut up -Wall
-			break;
-		}
+	}
+	if (!symtab_shdr) {
+		LOG_MSG("Symbol table not found")
+		return false;
 	}
 
-	size_t symcount = symtab_shdr->sh_size / symtab_shdr->sh_entsize;
 	const Elf64_Shdr *strtbl_shdr = shdr + symtab_shdr->sh_link;
 	const char *strtab = (const char *)(map->base + strtbl_shdr->sh_offset);
 	const Elf64_Sym *sym = (const Elf64_Sym *)(map->base + symtab_shdr->sh_offset);
 	const char *name = NULL;
 	idx = -1;
+	size_t symcount = symtab_shdr->sh_size / symtab_shdr->sh_entsize;
 	while (++idx < (int)symcount) {
 		if (sym->st_name == 0) {
 			sym++;
